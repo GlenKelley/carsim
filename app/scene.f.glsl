1f2c92e6399a32c9058f8dd031ed981dc9bb8cd6
Glen Kelley <glen.t.kelley@gmail.com>
2013-10-02 18:25:06 +1000

    nicer zebra shader

--- app/scene.f.glsl
+++ app/scene.f.glsl
@@ -7,6 +7,6 @@ out vec4 fragColor;
 void main()
 {
     vec4 t = textureCoord;
-    vec3 c = vec3(1,1,1) * (sin(20*atan(t.y,t.z))+0.5) * length(t.yz) / (t.x*t.x*10+1);
+    vec3 c = vec3(1,1,1) * clamp(20*sin(4*atan(t.y,t.z))-19,0,1)+vec3(0.2) * length(t.yz) / (t.x*t.x*10+1);
     fragColor = vec4(c, t.w);
 }
